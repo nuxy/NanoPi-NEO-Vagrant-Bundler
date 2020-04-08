d0caac2acaa37fcf82077e7d4e8e0b97d5b7f7b0
Marc S. Brooks <marc@mbrooks.info>
2020-04-07 23:04:41 -0700

    Removed chroot installed packages

--- .vagrant/install.sh
+++ .vagrant/install.sh
@@ -1,28 +1,3 @@
 #!/bin/sh
 
 pkg install -y ca_root_nss git picocom screen
-
-#------------------------------------------------------------------+
-# Qemu/Kernel
-#------------------------------------------------------------------+
-
-pkg install -y qemu-user-static u-boot-tools
-
-#------------------------------------------------------------------+
-# SQLite3
-#------------------------------------------------------------------+
-
-pkg install -y sqlite3
-
-#------------------------------------------------------------------+
-# NodeJS/Express
-#------------------------------------------------------------------+
-
-pkg install -y node12 npm-node12 python3
-
-# Install NPM package.
-cd /vagrant/server
-npm install --no-audit --no-optional > /dev/null 2>&1
-
-# Launch services.
-#screen -dm sudo -u vagrant npm run build ; npm run start
